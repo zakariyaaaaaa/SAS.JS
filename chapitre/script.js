510bf8552f2ccb7faf298b9a5ce9491743099723
Zakariya Boutlane <zakariyaboulane1@gmail.com>
2025-09-25 11:10:57 +0100

    Add files via upload

---
+++ chapitre/script.js
@@ -0,0 +1,39 @@
+while (true) {
+    let a = prompt("entre une mote comme ca / C1,C2,C3,C4,C5,C6,C7,C8")
+    if (a == null) {
+        alert("GOOOD BY BEBY")
+        break;
+    }
+    if (a == "exit") {
+        alert("WA BYY")
+        break;
+    }
+    switch (a) {
+        case "C1":
+            alert("HEY C1")
+            break;
+        case "C2":
+            alert("HEY C2")
+            break;
+        case "C3":
+            alert("HEY C3")
+            break; 
+        case "C4":
+            alert("HEY C4")
+            break;
+        case "C5":
+            alert("HEY C5")
+            break;
+        case "C6":
+            alert("HEYC6")
+            break;
+        case "C7":
+            alert("HEY C7")
+            break;
+        case "C8":
+            alert("HEY C8")
+            break;
+        default:
+            alert("3wd t3wd t7a9a9 mn code")
+    }
+}
